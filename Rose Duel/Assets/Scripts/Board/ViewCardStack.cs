1dbb18be9b9580d8805cfdef587669042fc2eaac
Alvin Stilley <111103467+AStilley@users.noreply.github.com>
2024-04-22 10:57:01 -0500

    Small update
    Added UI element for the start of the player hand

--- Rose Duel/Assets/Scripts/Board/ViewCardStack.cs
+++ Rose Duel/Assets/Scripts/Board/ViewCardStack.cs
@@ -44,10 +44,7 @@ public class ViewCardStack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
 
-        }
     }
     public void ClearList()
     {
